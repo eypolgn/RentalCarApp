751f5c0acf4aa056f89a76dc17c6f40fb536050a
eypolgn <eyup48244@gmail.com>
2024-08-06 16:17:42 +0300

    yeni

--- RentalCar/src/main/java/com/example/RentalCar/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.RentalCar.repository;
-
-import com.example.RentalCar.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Integer> {
-}
